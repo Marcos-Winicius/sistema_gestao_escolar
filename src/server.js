ab9259f9f42bf902f158a59fbefa99558b221162
Marcos-Winicius <marcoswini990@gmail.com>
2025-01-15 02:11:10 -0300

    Criação do index.html e configurando uso dos arquivos estáticos

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {resolve} = require('path')
 const db = require('./config/db')
 const disciplinaRoutes = require('./routes/disciplinaRoutes')
 
@@ -7,13 +8,14 @@ const app = express();
 const port = 8080
 const host = 'localhost'
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.static(resolve('./src/public')));
 // Rotas
 app.use('/disciplinas', disciplinaRoutes);
 
-app.get('/', (req, res)=>{
-    res.send('<h1>Teste</h1>')
-})
+// app.get('/', (req, res)=>{
+//     res.send('<h1>Teste</h1>')
+// })
 
 app.listen(port, host, ()=>{
     console.log(`Servidor inicializado no endereço: http://${host}:${port}`)
