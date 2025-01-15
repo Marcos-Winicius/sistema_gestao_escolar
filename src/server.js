cc7df799b11960e48e6240d9caad46bb711e6655
Marcos-Winicius <marcoswini990@gmail.com>
2025-01-15 00:41:22 -0300

    Primeiro commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const port = 8080
+const host = 'localhost'
+
+app.get('/', (req, res)=>{
+    res.send('<h1>Teste</h1>')
+})
+
+app.listen(port, host, ()=>{
+    console.log(`Servidor inicializado no endereço: http://${host}:${port}`)
+})
