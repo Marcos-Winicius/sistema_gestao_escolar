985d6035404e6ab4bd90acd81ba75adfcbbca0a8
Marcos Winicius Costa da Silva Pedro <138156579+Marcos-Winicius@users.noreply.github.com>
2025-03-05 04:31:20 +0000

    Corrigindo criação do código do curso

--- src/controllers/cursosController.js
+++ src/controllers/cursosController.js
@@ -15,7 +15,7 @@ module.exports = {
       res.status(500).json({ error: "Erro ao buscar cursos!" });
     }
   },
-
+  
   getById: async (req, res) => {
     const { codigo } = req.params;
     if (!codigo) {
@@ -33,20 +33,28 @@ module.exports = {
       res.status(500).json({ error: "Erro ao buscar curso!" });
     }
   },
-
+  
   create: async (req, res) => {
     try {
       const { nome, descricao, ch, nivel, modalidade } = req.body;
-      const codigo = uuid4();
-
+      
+      // Gerar um código único de 5 dígitos
+      let codigo;
+      let cursoExistente;
+      
+      do {
+        codigo = Math.floor(10000 + Math.random() * 90000); // Garante um número entre 10000 e 99999
+        cursoExistente = await Cursos.findOne({ where: { codigo } });
+      } while (cursoExistente); // Garante que o código gerado não está duplicado
+      
       await Cursos.create({ codigo, nome, descricao, ch, nivel, modalidade });
-      res.status(201).json({ msg: "Curso criado com sucesso!" });
+      res.status(201).json({ msg: "Curso criado com sucesso!", codigo });
     } catch (error) {
       console.error("Erro ao criar curso!", error);
       res.status(500).json({ error: "Erro ao criar curso!" });
     }
   },
-
+  
   updateCurso: async (req, res) => {
     const { codigo } = req.params;
     if (!codigo) {
@@ -65,7 +73,7 @@ module.exports = {
       res.status(500).json({ error: "Erro ao atualizar curso!" });
     }
   },
-
+  
   deleteCurso: async (req, res) => {
     const { codigo } = req.params;
     if (!codigo) {
